eaaeb990a61e70f9c949f7cb7597b7cd5fee58ff
dantelt24 <dlacey-thompson@csumb.edu>
2017-02-07 18:57:43 +0000

    Adding functioning image loading

--- proj1.py
+++ proj1.py
@@ -1,6 +1,15 @@
 from PIL import Image
 import sys
+import os
+import glob
 
-print "Hello, World"
+img_list=[]
+for filename in glob.glob('/home/ubuntu/workspace/cst205proj1/images/*.png'): #regex looking for images that end in .png format, needed to use absolute path not relative
+        img=Image.open(filename)
+        img_list.append(img)
+        print(img.format, img.size, img.mode)#tesing image info
+print len(img_list)#check if all images were loaded.
+#/cst205proj1/images path
+#print "Hello, World"
 
 #git pracfile
