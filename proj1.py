f2ecc882c8dc1ba9e360a7ff9147cff9ed601b4b
dantelt24 <dlacey-thompson@csumb.edu>
2017-02-07 19:00:29 +0000

    renamed imgs folder

--- proj1.py
+++ proj1.py
@@ -4,12 +4,13 @@ import os
 import glob
 
 img_list=[]
-for filename in glob.glob('/home/ubuntu/workspace/cst205proj1/images/*.png'): #regex looking for images that end in .png format, needed to use absolute path not relative
+for filename in glob.glob('/home/ubuntu/workspace/cst205proj1/Proj1images/*.png'): #regex looking for images that end in .png format, needed to use absolute path not relative
         img=Image.open(filename)
         img_list.append(img)
         print(img.format, img.size, img.mode)#tesing image info
 print len(img_list)#check if all images were loaded.
-#/cst205proj1/images path
+#/cst205proj1/images  -relative path
 #print "Hello, World"
 
 #git pracfile
+
