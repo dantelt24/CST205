a73b6777ec1e49109d2815418df91fe6415c53b5
dantelt24 <dlacey-thompson@csumb.edu>
2017-02-07 20:20:04 +0000

    added median func

--- proj1.py
+++ proj1.py
@@ -1,7 +1,15 @@
+'''
+Dante Lacey-Thompson
+CST205-MultiMedia Programming
+Project1
+This project will allow the user to filter out a certain thing that they wouldn't like in a series of pics.
+It will do this by loading the images, applying a median filter, and then using that to create the new image.
+'''
+
 from PIL import Image
 import sys
 import os
-import glob
+import glob #glob allows for unix-style filename pattern matching, allowing you to use regex to find the files you would like in python
 
 img_list=[]
 for filename in glob.glob('/home/ubuntu/workspace/cst205proj1/Proj1images/*.png'): #regex looking for images that end in .png format, needed to use absolute path not relative
@@ -18,3 +26,13 @@ print len(img_list)#check if all images were loaded.
 
 #git pracfile
 
+def getMedian(lst_obj): #find median values 
+    srtlist = sorted(lst_obj)
+    lst_len = len(lst_obj)
+    index = (lst_len-1) / 2
+    if(lst_len%2):
+        return srtlist[index]
+    else:
+        return (srtlist[index] + srtlist[index+1])/2
+
+        
